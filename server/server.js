7b37e1198bd11b133b2eccc6ea2d94f9a2aa736e
unknown <yordimosq@hotmail.com>
2018-11-18 22:53:25 -0500

    Se inicia el proyecto

---
+++ server/server.js
@@ -0,0 +1,39 @@
+require('./config/config');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', function (req, res) {
+    res.json('get Usuario')
+});
+
+app.post('/usuario', function (req, res) {
+    let body = req.body;
+    if (!body.nombre) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        })
+    }
+    else {
+        res.json({ persona: body })
+    }
+});
+
+app.put('/usuario/:id', function (req, res) {
+    let id = request.params.id;
+    res.json({
+        id
+    })
+});
+
+app.delete('/usuario', function (req, res) {
+    res.json('delete Usuario')
+});
+
+app.listen(process.env.PORT, () => { console.log('Escuchando puerto', 3000) });
